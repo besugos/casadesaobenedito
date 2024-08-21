413f033c3df9b40f0129dc8aec08f297ba199068
wendel <besugos@gmail.com>
2024-08-21 17:07:29 -0300

    uploading starter files

---
+++ js/navbar.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    $('.nav-link').removeClass('active');
+
+    let currentPage = window.location.pathname;
+
+    if (currentPage.includes('health')) {
+        $('#nav-health').addClass('active');
+    } else if (currentPage.includes('posts')) {
+        $('#nav-posts').addClass('active');
+    } else if (currentPage.includes('cabala')) {
+        $('#nav-cabala').addClass('active');
+    } else if (currentPage.includes('buzios')) {
+        $('#nav-buzios').addClass('active');
+    } else {
+        $('#nav-home').addClass('active');
+    }
+
+
+});
